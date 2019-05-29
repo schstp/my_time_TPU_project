71240bc1e5095ec3cea0a11ba104a86e3cd216f0
Stas <schstp@gmail.com>
2019-05-29 12:24:42 +0700

    last_term_start_point

--- blog/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class CommentForm(forms.Form):
-    comment = forms.CharField(widget = forms.Textarea(attrs={'cols': 40, 'rows':5, 'style':'resize:none;',
-    'placeholder': "Присоединяйтесь к обсуждению...", 'required': True}), label = "")
--- feedback/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('', views.contactForm, name = 'guest-support'),
-    path('thanks', views.thanks, name = 'thanks'),
-]
