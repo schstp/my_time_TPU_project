9e0c7b655bdd193a5140e7e9254eea2c8a359b34
Stas <schstp@gmail.com>
2019-06-02 09:20:31 +0700

    before migrations

--- mytime/main/models.py
+++ mytime/main/models.py
@@ -2,27 +2,32 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class List(models.Model):
-    title = models.CharField(max_length=200)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+class TimeStampedModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True, auto_now= False)
+    modified = models.DateTimeField(auto_now=True, auto_now_add=False)
 
     class Meta:
         abstract = True
 
 
-class InboxList(List):
-    pass
-
-
-class Task(models.Model):
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
-    content = models.TextField()
-    lists = models.ManyToManyField(List)
-    timestemp = models.DateTimeField(auto_now_add = True, auto_now = False)
-
-
-    def __str__(self):
-        return self.content
+class List(TimeStampedModel):
+    title = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
-        ordering = ['-timestemp']
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
+
+
+class Task(TimeStampedModel):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    title = models.TextField()
+    lists = models.ForeignKey(List, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
